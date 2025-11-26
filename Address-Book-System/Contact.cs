99d6a2f95071247a46475f264562816ee3042490
mr-vinay02 <vinayvini4630@gmail.com>
2025-11-26 15:10:58 +0530

    This is a Contact

--- Address-Book-System/Contact.cs
+++ Address-Book-System/Contact.cs
@@ -8,5 +8,18 @@ namespace Address_Book_System
 {
     internal class Contact
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {FirstName} {LastName}\nAddress: {Address}, {City}, {State}, {Zip}\nPhone: {PhoneNumber}\nEmail: {Email}";
+        }
     }
 }
