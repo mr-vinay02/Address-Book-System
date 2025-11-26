f88c4a5919842d83540ee2c18df2558a250fc2e7
mr-vinay02 <vinayvini4630@gmail.com>
2025-11-26 15:26:39 +0530

    This is service : Add Contact

--- Address-Book-System/AddressBookService.cs
+++ Address-Book-System/AddressBookService.cs
@@ -8,5 +8,72 @@ namespace Address_Book_System
 {
     internal class AddressBook_
     {
+
+        private List<Contact> contacts = new List<Contact>();
+
+        // UC2 – Add Contact
+        public void AddContact()
+        {
+            Contact c = new Contact();
+
+            Console.Write("Enter First Name: ");
+            c.FirstName = Console.ReadLine();
+
+            Console.Write("Enter Last Name: ");
+            c.LastName = Console.ReadLine();
+
+            Console.Write("Enter Address: ");
+            c.Address = Console.ReadLine();
+
+            Console.Write("Enter City: ");
+            c.City = Console.ReadLine();
+
+            Console.Write("Enter State: ");
+            c.State = Console.ReadLine();
+
+            Console.Write("Enter Zip: ");
+            c.Zip = Console.ReadLine();
+
+            Console.Write("Enter Phone Number: ");
+            c.PhoneNumber = Console.ReadLine();
+
+            Console.Write("Enter Email: ");
+            c.Email = Console.ReadLine();
+
+            contacts.Add(c);
+            Console.WriteLine("Contact Added Successfully!");
+        }
+
+        // UC3 – Edit Contact
+        public void EditContact(string name)
+        {
+            Contact contact = contacts.FirstOrDefault(c => c.FirstName.Equals(name, StringComparison.OrdinalIgnoreCase));
+
+            if (contact == null)
+            {
+                Console.WriteLine("Contact Not Found!");
+                return;
+            }
+
+            Console.Write("Enter New Address: ");
+            contact.Address = Console.ReadLine();
+
+            Console.Write("Enter New City: ");
+            contact.City = Console.ReadLine();
+
+            Console.Write("Enter New State: ");
+            contact.State = Console.ReadLine();
+
+            Console.Write("Enter New Zip: ");
+            contact.Zip = Console.ReadLine();
+
+            Console.Write("Enter New Phone Number: ");
+            contact.PhoneNumber = Console.ReadLine();
+
+            Console.Write("Enter New Email: ");
+            contact.Email = Console.ReadLine();
+
+            Console.WriteLine("Contact Updated Successfully!");
+        }
     }
 }
