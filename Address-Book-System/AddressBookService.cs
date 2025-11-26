364249445076d37fd69f67d96e17d8aaa05c98c4
mr-vinay02 <vinayvini4630@gmail.com>
2025-11-26 15:37:52 +0530

    This is a Service : Delete Contact

--- Address-Book-System/AddressBookService.cs
+++ Address-Book-System/AddressBookService.cs
@@ -76,6 +76,21 @@ namespace Address_Book_System
             Console.WriteLine("Contact Updated Successfully!");
         }
 
+        // UC4 – Delete Contact
+        public void DeleteContact(string name)
+        {
+            var person = contacts.FirstOrDefault(c => c.FirstName.Equals(name, StringComparison.OrdinalIgnoreCase));
+
+            if (person == null)
+            {
+                Console.WriteLine("Contact Not Found!");
+                return;
+            }
+
+            contacts.Remove(person);
+            Console.WriteLine("Contact Deleted Successfully!");
+        }
+
 
     }
 }
