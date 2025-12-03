c8f0a3b7c310e9157bcafe346cd44e82db4e146c
mr-vinay02 <vinayvini4630@gmail.com>
2025-12-03 15:04:11 +0530

    This is zip Custom Exception

---
+++ Address-Book-System/InvalidZipException.cs
@@ -0,0 +1,16 @@
+﻿
+// uc 6 custom exception
+namespace Address_Book_System
+{
+    
+    internal class InvalidZipException : Exception
+    {
+       
+
+        public InvalidZipException(string? message) : base(message)
+        {
+        }
+
+       
+    }
+}
